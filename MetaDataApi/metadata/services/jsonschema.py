f99002a2994f483ef55b53d6309dd44c20e471bb
William Sandvej Hansen <grusinator@gmail.com>
2018-09-27 07:53:11 +0200

    minor

--- MetaDataApi/metadata/services/jsonschema.py
+++ MetaDataApi/metadata/services/jsonschema.py
@@ -16,17 +16,28 @@ class JsonSchemaService():
         filename = url.split("/")[-1]
         baseurl = "/".join(url.split("/")[:-1])
 
-    return baseurl, filename
+        return baseurl, filename
 
     def load_json_schema(self, url):
         baseurl, filename = self.infer_schema_info_from_url(url)
 
         data = self.read_json_from_url(url)
 
-        navigation_dict = {filename: data, }
-        while navigation_dict.count is not 0:
-            nav_object = navigation_dict.pop()
+        navigation_dict = {filename: data}
+        while len(navigation_dict) is not 0:
+            #
+            _, nav_object = navigation_dict.popitem()
             for key, value in nav_object.items():
+
+                # if the element is a dict traverse it later
                 if isinstance(value, dict):
                     navigation_dict[key] = value
                     continue
+                # if element is a reference to another file
+                elif key is "$ref":
+                    ref_url = baseurl + value
+                    ref_data = self.read_json_from_url(ref_url)
+                    navigation_dict[value] = ref_data
+
+                elif isinstance(value, str):
+                    pass
