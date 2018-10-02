291c8b0b48b22d2f5cd5c88c3617f93fd3c748d5
William Sandvej Hansen <grusinator@gmail.com>
2018-10-02 12:54:23 +0200

    intermediate commit changing structure so that type==object dictates

--- MetaDataApi/metadata/services/jsonschema.py
+++ MetaDataApi/metadata/services/jsonschema.py
@@ -14,12 +14,12 @@ class JsonSchemaService():
         self.subtypes = ["oneOf", "allOf", "anyOf"]
         self.skip_fields = [
             "description",
-            "label",
+            # "label",
             "$schema",
-            "type",
             "definitions"
         ]
         self.skip_fields.extend(self.subtypes)
+        self._debug_objects_list = []
 
     def read_json_from_url(self, url):
 
@@ -55,19 +55,33 @@ class JsonSchemaService():
                 url=self.schema.url,
                 label=label)
             obj_rel.save()
+            # to check the objects that have been create
+            self._debug_objects_list.append(obj_rel)
 
     def create_attributes(self, no_obj_attributes, current_object):
         for no_obj_attribute in no_obj_attributes:
             no_obj_attribute.object = current_object
-            no_obj_attribute.save()
+            try:
+                no_obj_attribute.save()
+            except Exception as e:
+                # most likey if it allready exists
+                pass
+
+            # to check the objects that have been create
+            self._debug_objects_list.append(no_obj_attribute)
         return no_obj_attributes
 
     def validate_url(self, url):
         urlmapper = {
-            'http://tools.ietf.org/html/rfc3339': None,
+            "http://tools.ietf.org/html/rfc3339": None,
+            "http://purl.bioontology.org/ontology/SNOMEDCT/7389001": None
         }
         var1 = url in urlmapper
         return urlmapper.get(url) if url in urlmapper else url
+
+    def filt_type(self, obj_list, object_class):
+        return list(
+            filter(lambda x: isinstance(x, object_class), obj_list))
 
     def identify_properties(self, dict_data, root_label, current_object=None,
                             definitions=None):
@@ -79,6 +93,16 @@ class JsonSchemaService():
         obj_type = dict_data.get("type")
 
         description = dict_data.get("description")
+
+        # first figure out if this is a class
+        if dict_data.get("type") == "object":
+            current_object = Object(
+                label=root_label,
+                schema=self.schema,
+                description=description)
+            current_object.save()
+            # to check the objects that have been create
+            self._debug_objects_list.append(current_object)
 
         # this basicly ignores the oneof.. etc. structure
         subdata = [dict_data.get(subtype) for subtype in self.subtypes]
@@ -103,7 +127,9 @@ class JsonSchemaService():
                         url = self.baseurl + "/" + \
                             definition["$ref"]
                     elif "references" in definition:
-                        url = definition.get("references")[0].get("url")
+                        continue
+                        # skip references
+                        # url = definition.get("references")[0].get("url")
 
                 else:
                     url = self.baseurl + "/" + value
@@ -115,32 +141,55 @@ class JsonSchemaService():
                 # load data and prepare to iterate trough
                 new_dict_data = self.read_json_from_url(url)
 
+                # when stepping into a new file the label should be
+                # the file name
+
+                _, filename = self.infer_schema_info_from_url(url)
+                label = filename.replace(".json", "")
                 # definitions is not inherited trough references
                 new_objects = self.identify_properties(
-                    new_dict_data, root_label, current_object)
+                    new_dict_data, label, current_object)
+                return_objects.extend(new_objects)
 
-            # probably a attribute but the one below will handle it
+            # only select type if it is not an object type
+            elif key in ["value", "type"] and value != "object":
+                # this is an attribute
+
+                # try to find the relevant attribute date
+                unit = dict_data.get("unit")
+                data_type = dict_data.get("type")
+                description = dict_data.get("description")
+                if isinstance(value, dict):
+                    unit = value.get("unit") or unit
+                    data_type = value.get("type") or data_type
+                    description = value.get("description") or description
+
+                # if we have allready found an attribute
+                if len(self.filt_type(return_objects, Attribute)):
+                    attribute = next(self.filt_type(return_objects, Attribute))
+                    # update missing info
+                    attribute.datatype = attribute.datatype or data_type
+                    attribute.description = attribute.description or description
+                    # root label should be okay
+                else:
+                    # here we just create it and return it
+                    # saving is done after the object has been created
+                    # if this dict contains "other classes"
+                    attribute = Attribute(
+                        label=root_label,
+                        datatype=data_type,
+                        description=description,
+                        object=current_object
+                    )
+                    # consider saving here if it gets an object with
+                    return_objects.append(attribute)
+
+            # this is an attribute, but if we allready have an attribute in the
+            # return object, add these info to that one, else just create one maybe
             elif key == "unit":
                 pass
             elif key == "enum":
                 pass
-            elif key == "value":
-                # this is an attribute
-
-                # try to find the unit
-                unit = dict_data.get("unit")
-                data_type = value.get("type")
-                description = value.get("description")
-
-                # here we just create it and return it
-                # saving is done after the object has been created
-                # if this dict contains "other classes"
-                attribute = Attribute(
-                    label=root_label,
-                    datatype=data_type,
-                    object=None
-                )
-                return_objects.append(attribute)
 
             elif key == "properties":
 
@@ -150,37 +199,46 @@ class JsonSchemaService():
                 # by iterating though properties we dont know if it is an object
                 # or just an attribute (if it only has unit and value)
 
-                no_obj_attributes = list(
-                    filter(lambda x: isinstance(x, Attribute), new_objects))
-                objects = list(
-                    filter(lambda x: isinstance(x, Object), new_objects))
+                no_obj_attributes = self.filt_type(new_objects, Attribute)
+                objects = self.filt_type(new_objects, Object)
 
                 # it is an attribute
-                if len(no_obj_attributes) == 1 and len(objects) == 0:
+                if len(no_obj_attributes) >= 1 and len(objects) == 0:
                     attributes = self.create_attributes(
                         no_obj_attributes, current_object)
-                    return_objects.extend(attributes)
+                    # return_objects.extend(attributes)
 
                     # if this is caracterized as attrbute
                     # it cannot be a class, maybe return instead
                     continue
 
                 # this is an object, so lets create it
+                if len(new_objects):
+                    # we know this is an object because it has properties
+                    current_object = Object(
+                        label=root_label,
+                        schema=self.schema,
+                        description=description)
+                    current_object.save()
+                    # to check the objects that have been create
+                    self._debug_objects_list.append(current_object)
 
-                # we know this is an object because it has properties
-                current_object = Object(
-                    label=root_label,
-                    schema=self.schema,
-                    description=description)
-                current_object.save()
+                    # newly discovered objects should be added to return list
+                    return_objects.append(current_object)
 
-                # newly discovered objects should be added to return list
-                return_objects.append(current_object)
+                    # create the object relations with the current and the returned objects
+                    self.create_object_relations(current_object, objects)
 
-                # create the object relations with the current and the returned objects
-                self.create_object_relations(current_object, objects)
+                # if there is no object this is a dead end. we dont want to create an object
+                # this also means that objects that have no attributes will not be created
+                # consider if this is what we want.
+
+                continue
 
             elif isinstance(value, dict):
+                if len(self._debug_objects_list) >= 3:
+                    pass
+
                 new_objects = self.identify_properties(
                     value, key, current_object, definitions)
                 return_objects.extend(new_objects)
