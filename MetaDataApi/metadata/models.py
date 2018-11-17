7e6120328b6a0f7724fe85ce2fc98941aac17013
William Sandvej Hansen <grusinator@gmail.com>
2018-11-17 10:00:57 +0100

    restructured the models in admin

--- MetaDataApi/metadata/models.py
+++ MetaDataApi/metadata/models.py
@@ -1,162 +1,162 @@
-from django.db import models
-from MetaDataApi.metadata.custom_storages import MediaStorage
-from datetime import datetime
-# Create your models here.
+# from django.db import models
+# from MetaDataApi.metadata.custom_storages import MediaStorage
+# from datetime import datetime
+# # Create your models here.
 
 
-class Schema(models.Model):
+# class Schema(models.Model):
 
-    label = models.TextField(unique=True)
-    description = models.TextField(null=True, blank=True)
-    url = models.URLField(unique=True)
-    rdfs_file = models.FileField(
-        upload_to="schemas",
-        null=True, blank=True, storage=MediaStorage())
+#     label = models.TextField(unique=True)
+#     description = models.TextField(null=True, blank=True)
+#     url = models.URLField(unique=True)
+#     rdfs_file = models.FileField(
+#         upload_to="schemas",
+#         null=True, blank=True, storage=MediaStorage())
 
-    def __str__(self):
-        return "S:" + self.label
+#     def __str__(self):
+#         return "S:" + self.label
 
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.url == other.url and \
-                self.label == other.label
-        else:
-            return False
+#     def __eq__(self, other):
+#         if isinstance(other, self.__class__):
+#             return self.url == other.url and \
+#                 self.label == other.label
+#         else:
+#             return False
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+#     def __ne__(self, other):
+#         return not self.__eq__(other)
 
-    class Meta:
-        app_label = 'metadata'
-
-
-class Object(models.Model):
-    label = models.TextField()
-    description = models.TextField(null=True, blank=True)
-    # related name should not be objects, because that will cause problems
-    schema = models.ForeignKey(
-        Schema, related_name='object_list', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "O:%s.%s" % (self.schema.label, self.label)
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            # return self.__dict__ == other.__dict__
-
-            def inner_join_by_label(from1, from2):
-                from1 = [elm.label for elm in from1]
-                from2 = [elm.label for elm in from2]
-
-                commons = list(set(from1) & set(from2))
-                return any(commons)
-
-            sel_f = hasattr(self, "from_objects")
-            oth_f = hasattr(other, "from_objects")
-
-            if sel_f and oth_f:
-                from_obj_match = inner_join_by_label(
-                    self.from_objects,
-                    other.from_objects)
-            else:
-                # if they are not equal
-                from_obj_match = sel_f == oth_f
-
-            return self.schema == other.schema and \
-                self.label == other.label and \
-                from_obj_match
-
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    class Meta:
-        unique_together = ('label', 'schema')
-        app_label = 'metadata'
+#     class Meta:
+#         app_label = 'metadata'
 
 
-class Attribute(models.Model):
-    data_type_map = {
-        datetime: "datetime",
-        float: "float",
-        int: "int",
-        bool: "bool",
-        str: "string",
-        type(None): "unknown"
-    }
-    data_type_choises = [(x, x) for x in data_type_map.values()]
+# class Object(models.Model):
+#     label = models.TextField()
+#     description = models.TextField(null=True, blank=True)
+#     # related name should not be objects, because that will cause problems
+#     schema = models.ForeignKey(
+#         Schema, related_name='object_list', on_delete=models.CASCADE)
 
-    # db Fields
-    label = models.TextField()
-    description = models.TextField(null=True, blank=True)
-    datatype = models.TextField(choices=data_type_choises)
-    dataunit = models.TextField()
-    object = models.ForeignKey(
-        Object, related_name='attributes', on_delete=models.CASCADE)
+#     def __str__(self):
+#         return "O:%s.%s" % (self.schema.label, self.label)
 
-    def __str__(self):
-        return "A:%s.%s" % (self.object.label, self.label)
+#     def __eq__(self, other):
+#         if isinstance(other, self.__class__):
+#             # return self.__dict__ == other.__dict__
 
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
+#             def inner_join_by_label(from1, from2):
+#                 from1 = [elm.label for elm in from1]
+#                 from2 = [elm.label for elm in from2]
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+#                 commons = list(set(from1) & set(from2))
+#                 return any(commons)
 
-    @staticmethod
-    def assert_datatype(attribute, datatype):
-        datatype_name = Attribute.data_type_map[datatype]
-        assert attribute.datatype == datatype_name, \
-            "the found attr: %s has type %s, but it must be a %s" % (
-                attribute.label,
-                attribute.datatype,
-                str(datatype_name)
-            )
+#             sel_f = hasattr(self, "from_objects")
+#             oth_f = hasattr(other, "from_objects")
 
-    class Meta:
-        unique_together = ('label', 'object')
-        app_label = 'metadata'
+#             if sel_f and oth_f:
+#                 from_obj_match = inner_join_by_label(
+#                     self.from_objects,
+#                     other.from_objects)
+#             else:
+#                 # if they are not equal
+#                 from_obj_match = sel_f == oth_f
 
+#             return self.schema == other.schema and \
+#                 self.label == other.label and \
+#                 from_obj_match
 
-class ObjectRelation(models.Model):
-    label = models.TextField()
-    description = models.TextField(null=True, blank=True)
-    schema = models.ForeignKey(
-        Schema, related_name='object_relations', on_delete=models.CASCADE)
-    from_object = models.ForeignKey(
-        Object, related_name='to_relations', on_delete=models.CASCADE)
-    to_object = models.ForeignKey(
-        Object, related_name='from_relations', on_delete=models.CASCADE)
+#         else:
+#             return False
 
-    def __str__(self):
-        return "R:%s - %s - %s" % (
-            self.from_object.label,
-            self.label,
-            self.to_object.label)
+#     def __ne__(self, other):
+#         return not self.__eq__(other)
 
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    class Meta:
-        unique_together = ('label', 'schema')
-        app_label = 'metadata'
+#     class Meta:
+#         unique_together = ('label', 'schema')
+#         app_label = 'metadata'
 
 
-class UnmappedObject(models.Model):
-    label = models.TextField()
-    parrent_label = models.TextField()
-    childrens = models.TextField()
+# class Attribute(models.Model):
+#     data_type_map = {
+#         datetime: "datetime",
+#         float: "float",
+#         int: "int",
+#         bool: "bool",
+#         str: "string",
+#         type(None): "unknown"
+#     }
+#     data_type_choises = [(x, x) for x in data_type_map.values()]
 
-    class Meta:
-        app_label = 'metadata'
+#     # db Fields
+#     label = models.TextField()
+#     description = models.TextField(null=True, blank=True)
+#     datatype = models.TextField(choices=data_type_choises)
+#     dataunit = models.TextField()
+#     object = models.ForeignKey(
+#         Object, related_name='attributes', on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return "A:%s.%s" % (self.object.label, self.label)
+
+#     def __eq__(self, other):
+#         if isinstance(other, self.__class__):
+#             return self.__dict__ == other.__dict__
+#         else:
+#             return False
+
+#     def __ne__(self, other):
+#         return not self.__eq__(other)
+
+#     @staticmethod
+#     def assert_datatype(attribute, datatype):
+#         datatype_name = Attribute.data_type_map[datatype]
+#         assert attribute.datatype == datatype_name, \
+#             "the found attr: %s has type %s, but it must be a %s" % (
+#                 attribute.label,
+#                 attribute.datatype,
+#                 str(datatype_name)
+#             )
+
+#     class Meta:
+#         unique_together = ('label', 'object')
+#         app_label = 'metadata'
+
+
+# class ObjectRelation(models.Model):
+#     label = models.TextField()
+#     description = models.TextField(null=True, blank=True)
+#     schema = models.ForeignKey(
+#         Schema, related_name='object_relations', on_delete=models.CASCADE)
+#     from_object = models.ForeignKey(
+#         Object, related_name='to_relations', on_delete=models.CASCADE)
+#     to_object = models.ForeignKey(
+#         Object, related_name='from_relations', on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return "R:%s - %s - %s" % (
+#             self.from_object.label,
+#             self.label,
+#             self.to_object.label)
+
+#     def __eq__(self, other):
+#         if isinstance(other, self.__class__):
+#             return self.__dict__ == other.__dict__
+#         else:
+#             return False
+
+#     def __ne__(self, other):
+#         return not self.__eq__(other)
+
+#     class Meta:
+#         unique_together = ('label', 'schema')
+#         app_label = 'metadata'
+
+
+# class UnmappedObject(models.Model):
+#     label = models.TextField()
+#     parrent_label = models.TextField()
+#     childrens = models.TextField()
+
+#     class Meta:
+#         app_label = 'metadata'
--- MetaDataApi/metadata/models/instances.py
+++ MetaDataApi/metadata/models/instances.py
@@ -34,6 +34,15 @@ class CategoryTypes(Enum):
 """ used for uploading data before processed into structured data"""
 
 
+class BaseInstance(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              null=True, on_delete=models.CASCADE)
+    base = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
 class RawData(models.Model):
     starttime = models.DateTimeField(auto_now=False)
     stoptime = models.DateTimeField(null=True, blank=True)
@@ -53,7 +62,7 @@ class RawData(models.Model):
             self.starttime.strftime("%Y-%m-%d %H:%M:%S"))
 
     class Meta:
-        app_label = 'datapoints'
+        app_label = 'metadata'
 
 
 class RDFDataDump(models.Model):
@@ -67,32 +76,23 @@ class RDFDataDump(models.Model):
 # instance classes
 
 
-class ObjectInstance(models.Model):
-    base = models.ForeignKey(
-        Object,
-        related_name='object_instances', on_delete=models.CASCADE)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              null=True, on_delete=models.CASCADE)
+class ObjectInstance(BaseInstance):
 
     def __str__(self):
         return "Oi:%s.%s" % (self.base.schema.label, self.base.label)
 
     class Meta:
-        app_label = 'datapoints'
+        app_label = 'metadata'
+        default_related_name = '%(model_name)s'
 
 
-class ObjectRelationInstance(models.Model):
-    base = models.ForeignKey(
-        ObjectRelation,
-        related_name='object_relation_instances', on_delete=models.CASCADE)
+class ObjectRelationInstance(BaseInstance):
     from_object = models.ForeignKey(
         ObjectInstance,
         related_name='to_relations', on_delete=models.CASCADE)
     to_object = models.ForeignKey(
         ObjectInstance,
         related_name='from_relations', on_delete=models.CASCADE)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Ri:%s - %s - %s" % (
@@ -101,7 +101,8 @@ class ObjectRelationInstance(models.Model):
             self.base.to_object.label)
 
     class Meta:
-        app_label = 'datapoints'
+        app_label = 'metadata'
+        default_related_name = '%(model_name)s'
 
     # custom restrictions on foreign keys to make sure the instances are of
     # the right meta object type
@@ -117,9 +118,8 @@ class ObjectRelationInstance(models.Model):
 
 
 # this is the class that we are going to inherit from
-class BaseAttributeInstance(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              null=True, on_delete=models.CASCADE)
+class BaseAttributeInstance(BaseInstance):
+    object = models.ForeignKey(ObjectInstance, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Ai:%s.%s:%s" % (self.base.object.label, self.base.label,
@@ -134,74 +134,50 @@ class BaseAttributeInstance(models.Model):
 
         return super(BaseAttributeInstance, self).save(*args, **kwargs)
 
-    class Meta:
-        app_label = 'datapoints'
+    class Meta(BaseInstance.Meta):
+        abstract = True
 
 
-# we call the base attribute instance string because it is
-# the most generic one, just overwrite value, when inheriting
 class StringAttributeInstance(BaseAttributeInstance):
     value = models.TextField()
-    object = models.ForeignKey(
-        ObjectInstance, related_name="string_attributes",
-        on_delete=models.CASCADE)
-    base = models.ForeignKey(
-        Attribute,
-        related_name='string_instances', on_delete=models.CASCADE)
+
+    class Meta(BaseAttributeInstance.Meta):
+        default_related_name = '%(model_name)s'
 
 
 class DateTimeAttributeInstance(BaseAttributeInstance):
     # must have timestamp and value
     value = models.DateTimeField()
-    object = models.ForeignKey(
-        ObjectInstance, related_name="datetime_attributes",
-        on_delete=models.CASCADE)
-    base = models.ForeignKey(
-        Attribute,
-        related_name='date_time_instances', on_delete=models.CASCADE)
+
+    class Meta(BaseAttributeInstance.Meta):
+        default_related_name = '%(model_name)s'
 
 
 class BoolAttributeInstance(BaseAttributeInstance):
     # must have timestamp and value
     value = models.BooleanField()
-    base = models.ForeignKey(
-        Attribute, related_name="bool_attributes_insts",
-        on_delete=models.CASCADE)
-    object = models.ForeignKey(
-        ObjectInstance, related_name='bool_attributes',
-        on_delete=models.CASCADE)
-    base = models.ForeignKey(
-        Attribute,
-        related_name='bool_instances', on_delete=models.CASCADE)
+
+    class Meta(BaseAttributeInstance.Meta):
+        default_related_name = '%(model_name)s'
 
 
-class IntAttributeInstance(models.Model):
+class IntAttributeInstance(BaseAttributeInstance):
     # must have timestamp and value
     value = models.IntegerField()
-    base = models.ForeignKey(
-        Attribute, related_name="int_attributes_insts",
-        on_delete=models.CASCADE)
 
-    object = models.ForeignKey(
-        ObjectInstance, related_name='int_attributes',
-        on_delete=models.CASCADE)
+    class Meta(BaseAttributeInstance.Meta):
+        default_related_name = '%(model_name)s'
 
 
 class FloatAttributeInstance(BaseAttributeInstance):
     value = models.FloatField()
-    base = models.ForeignKey(
-        Attribute, related_name="float_attributes_insts",
-        on_delete=models.CASCADE)
-    object = models.ForeignKey(
-        ObjectInstance, related_name='float_attributes',
-        on_delete=models.CASCADE)
+
+    class Meta(BaseAttributeInstance.Meta):
+        default_related_name = '%(model_name)s'
 
 
 class ImageAttributeInstance(BaseAttributeInstance):
     value = models.ImageField()
-    base = models.ForeignKey(
-        Attribute, related_name="image_attribute_insts",
-        on_delete=models.CASCADE)
-    object = models.ForeignKey(
-        ObjectInstance, related_name='image_attributes',
-        on_delete=models.CASCADE)
+
+    class Meta(BaseAttributeInstance.Meta):
+        default_related_name = '%(model_name)s'
