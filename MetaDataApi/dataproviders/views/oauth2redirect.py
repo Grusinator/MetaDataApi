e72d773f74198761ec1144d1119b5874cc899c64
Grusinator <grusinator@gmail.com>
2019-04-19 20:15:13 +0200

    privacy policy, and terms of service. Oauth2redirect refactoring, and data fetch scheduling rdf schema

--- MetaDataApi/dataproviders/views/oauth2redirect.py
+++ MetaDataApi/dataproviders/views/oauth2redirect.py
@@ -1,10 +1,11 @@
-import json
 import logging
 
 import requests
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 
 from MetaDataApi.dataproviders.models import DataProvider
+from MetaDataApi.metadata.utils import JsonUtils
 from MetaDataApi.settings import OAUTH_REDIRECT_URI
 from MetaDataApi.users.models import DataProviderProfile
 from MetaDataApi.users.models import Profile
@@ -12,66 +13,99 @@ from MetaDataApi.users.models import Profile
 logger = logging.getLogger(__name__)
 
 
+class OauthRedirectRequestException(Exception):
+    pass
+
+
 def oauth2redirect(request):
-    # if not request.user.is_authenticated:
-    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    code = get_auth_code(request)
+    scope = get_scope(request)
+    profile = validate_and_get_profile(request)
+    data_provider = validate_and_get_provider(request)
 
+    access_token = request_acess_token(code, data_provider)
+
+    save_data_provider_profile(access_token, data_provider, profile)
+    return HttpResponse(
+        """successfully connected your profile with %s
+        <a href= "%s"> back <a> """
+        % (data_provider.provider_name,
+           "../providers/")
+    )
+
+
+def request_acess_token(code, data_provider):
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "client_id": data_provider.client_id,
+        "client_secret": data_provider.client_secret,
+        "redirect_uri": OAUTH_REDIRECT_URI,
+    }
+    url = data_provider.access_token_url
+    r = requests.post(url, data=data, allow_redirects=True)
+    if not r.ok:
+        raise OauthRedirectRequestException("the token request did not return with ok")
+
+    json_obj = JsonUtils.validate(r.content)
+
+    access_token = json_obj.pop("access_token")
+    token_type = json_obj.pop("token_type")
+
+    if access_token is "" or None:
+        raise OauthRedirectRequestException("access token was not found in response: %s" % json_obj)
+    return access_token
+
+
+def validate_and_get_provider(request):
+    provider_name, _ = get_state(request)
     try:
-        code = request.GET.get('code') or request.POST.get('code')
-        scope = request.GET.get('scope') or request.POST.get('scope')
-        state = request.GET.get('state') or request.POST.get('state')
-
-        provider_name, user_id = state.split(":")
-
-        data_provider = DataProvider.objects.get(
+        return DataProvider.objects.get(
             provider_name=provider_name)
+    except ObjectDoesNotExist as e:
+        logger.error(str(e))
+        raise OauthRedirectRequestException(
+            "state did not return a valid data provider object. provider: %s" % provider_name)
 
-        data = {
-            "grant_type": "authorization_code",
-            "code": code,
-            "client_id": data_provider.client_id,
-            "client_secret": data_provider.client_secret,
-            "redirect_uri": OAUTH_REDIRECT_URI,
-        }
 
-        url = data_provider.access_token_url
-        r = requests.post(url, data=data, allow_redirects=True)
+def get_state(request):
+    state = request.GET.get('state') or request.POST.get('state')
+    if not state or ":" not in state:
+        raise OauthRedirectRequestException("state was not parsed or invalid. state: %s" % str(state))
+    return state.split(":")
 
-        json_obj = json.loads(r.content)
 
-        errors = json_obj.get("errors")
-        if errors:
-            raise Exception(str(errors))
+def get_scope(request):
+    return request.GET.get('scope') or request.POST.get('scope')
 
-        access_token = json_obj.pop("access_token")
-        token_type = json_obj.pop("token_type")
 
-        try:
-            profile = request.user.profile
-        except:
-            profile = Profile.objects.get(user__pk=user_id)
+def get_auth_code(request):
+    code = request.GET.get('code') or request.POST.get('code')
+    return code
 
-        try:
-            tpp = DataProviderProfile.objects.get(
-                profile=profile, provider=data_provider)
-            tpp.access_token = access_token
-        except:
-            tpp = DataProviderProfile(
-                provider=data_provider,
-                access_token=access_token,
-                profile=profile,
-                profile_json_field=json_obj
-            )
 
-        tpp.save()
+def validate_and_get_profile(request):
+    _, user_id = get_state(request)
+    if not hasattr(request.user, "profile") or request.user.pk is None:
+        raise OauthRedirectRequestException("you are not logged in")
+    profile_on_user = request.user.profile
+    profile_by_id = Profile.objects.get(user__pk=user_id)
 
-        return HttpResponse(
-            """successfully connected your profile with %s
-            <a href= "%s"> back <a> """
-            % (provider_name,
-               "../providers/")
+    if profile_on_user.pk != profile_by_id.pk:
+        raise OauthRedirectRequestException("logged in profile do not match with the state")
+    return profile_on_user
+
+
+def save_data_provider_profile(access_token, data_provider, profile):
+    try:
+        tpp = DataProviderProfile.objects.get(
+            profile=profile, provider=data_provider)
+        tpp.access_token = access_token
+    except ObjectDoesNotExist:
+        tpp = DataProviderProfile(
+            provider=data_provider,
+            access_token=access_token,
+            profile=profile
         )
 
-    except Exception as e:
-        logger.error(e)
-        return HttpResponse("upps... something went wrong! (%s)" % str(e))
+    tpp.save()
---
+++ MetaDataApi/metadata/rdfs_models/rdfs_data_fetch_scheduling/rdfs_data_fetch_scheduling.py
@@ -0,0 +1,24 @@
+from MetaDataApi.metadata.models import (
+    Schema, Object, Attribute,
+    ObjectRelation)
+from MetaDataApi.metadata.rdfs_models import RdfsDataProvider
+from MetaDataApi.metadata.rdfs_models.base_rdfs_model import BaseRdfModel
+
+
+class RdfsDataFetchScheduling(BaseRdfModel):
+    class SchemaItems:
+        schema = Schema(label="meta_data_api")
+
+        data_fetch_schedule = Object(label="data_fetch_schedule", schema=schema)
+
+        has_data_fetch_schedule = ObjectRelation(
+            schema=schema,
+            label="has_data_fetch_schedule",
+            from_object=RdfsDataProvider.SchemaItems.rest_endpoint,
+            to_object=data_fetch_schedule
+        )
+        time_interval = Attribute(
+            label="data_provider_name",
+            object=data_fetch_schedule,
+            data_type=Attribute.DataType.String
+        )
