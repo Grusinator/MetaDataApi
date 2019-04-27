2ae1c680dea3d9c7e3d307c7e9281cf3d8ce74fc
Grusinator <grusinator@gmail.com>
2019-04-27 11:26:48 +0200

    fixed a reggression test

--- MetaDataApi/dataproviders/tests/services/test_urlFormatHelper.py
+++ MetaDataApi/dataproviders/tests/services/test_urlFormatHelper.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from unittest import TestCase
 
+from pytz import timezone
+
 from MetaDataApi.dataproviders.services.url_format_helper import UrlFormatHelper
 
 
 class TestUrlFormatHelper(TestCase):
     def test_build_args_for_url(self):
-        start_time = datetime(2018, 3, 4)
-        end_time = datetime(2018, 5, 4)
+        start_time = datetime(2018, 3, 4).astimezone(timezone("UTC"))
+        end_time = datetime(2018, 5, 4).astimezone(timezone("UTC"))
 
         endpoint = "testendpoint?start={StartDateTime:UTCSEC}&end={EndDateTime:UTCSEC}"
 
