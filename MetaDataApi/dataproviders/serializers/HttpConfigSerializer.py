59ad5617264258118810d9029f6a243b7577d197
William <grusinator@gmail.com>
2019-10-02 10:51:37 +0000

    created class from metaclass for httpconfig works with jsonfield. still errors in the generic one.

--- MetaDataApi/dataproviders/serializers/HttpConfigSerializer.py
+++ MetaDataApi/dataproviders/serializers/HttpConfigSerializer.py
@@ -6,6 +6,45 @@ from MetaDataApi.dataproviders.models.HttpConfig import HttpConfig
 class HttpConfigSerializer(serializers.ModelSerializer):
     header = serializers.JSONField()
     url_encoded_params = serializers.JSONField()
+
     class Meta:
         model = HttpConfig
         fields = ["header", "url_encoded_params"]
+
+    @classmethod
+    def build_from_metaclass(cls):
+        properties = {
+            "Meta": cls.build_nested_meta_class(),
+            "header": serializers.JSONField(),
+            "url_encoded_params" : serializers.JSONField()
+
+        }
+        return type("MetaHttpConfigSerializer", (serializers.ModelSerializer,), properties)
+
+    @classmethod
+    def build_nested_meta_class(cls):
+        properties = {
+            "model": HttpConfig,
+            "fields": ["header", "url_encoded_params"]
+        }
+        return type("Meta", (), properties)
+
+class SomeClass:
+    class Meta:
+        model = HttpConfig
+        fields = ["header", "url_encoded_params"]
+
+    @classmethod
+    def build_some_class(cls):
+        properties = {
+            "Meta": cls.build_Meta_class()
+        }
+        return type("someclass", (), properties)
+
+    @classmethod
+    def build_Meta_class(cls):
+        properties = {
+            "model": HttpConfig,
+            "fields" : ["header", "url_encoded_params"]
+        }
+        return type("Meta", (), properties)
--- MetaDataApi/dataproviders/tests/models/test_httpConfig.py
+++ MetaDataApi/dataproviders/tests/models/test_httpConfig.py
@@ -1,3 +1,5 @@
+import unittest
+
 import django
 from django.test import TransactionTestCase
 
@@ -32,7 +34,7 @@ class TestHttpConfig(TransactionTestCase):
         data = self.http.header
         self.assertEqual(expected, data)
 
-    def test_deserialize_http(self):
+    def test_http_dynamic_serializer_deserialize(self):
         data = {
             "http_config": {
                 "data_provider": {
@@ -55,7 +57,7 @@ class TestHttpConfig(TransactionTestCase):
                                      exclude=("dataproviderprofile", "data_provider", "data_provider_node"))
         self.assertEqual(obj, None)
 
-    def test_serialize_http(self):
+    def test_http_dynamic_serializer_serialize(self):
         expected = self.build_http_expected_json()
 
         http = self.build_http_model_objects()
@@ -63,7 +65,7 @@ class TestHttpConfig(TransactionTestCase):
         res = http.serialize(exclude=("body_type", "body_content", "data_provider", "request_type"))
         self.assertEqual(res, expected)
 
-    def test_http_serializer_deserialize(self):
+    def test_http_static_serializer_deserialize(self):
         from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
         data = self.build_http_expected_json()
         serializer = HttpConfigSerializer(data=data)
@@ -71,11 +73,85 @@ class TestHttpConfig(TransactionTestCase):
             raise Exception(f"could not deserialize, due to error: {serializer.errors}")
         self.assertEqual(serializer.validated_data, data)
 
-    def test_http_serializer_serialize(self):
+    def test_http_static_serializer_serialize(self):
         from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
         obj = self.build_http_model_objects()
         data = HttpConfigSerializer(obj).data
-        self.assertEqual(data, self.build_http_expected_json())
+        self.assertDictEqual(data, self.build_http_expected_json())
+
+    def test_http_static_meta_serializer_deserialize(self):
+        from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
+        data = self.build_http_expected_json()
+        MetaHttpConfigSerializer = HttpConfigSerializer.build_from_metaclass()
+        serializer = MetaHttpConfigSerializer(data=data)
+        if not serializer.is_valid():
+            raise Exception(f"could not deserialize, due to error: {serializer.errors}")
+        self.assertEqual(serializer.validated_data, data)
+
+    def test_http_static_meta_serializer_serialize(self):
+        from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
+        obj = self.build_http_model_objects()
+        data = HttpConfigSerializer.build_from_metaclass()(obj).data
+        self.assertDictEqual(data, self.build_http_expected_json())
+
+    def test_if_dynamic_serializer_class_is_equal_to_static(self):
+        from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
+        stat = HttpConfigSerializer
+        from MetaDataApi.dataproviders.models import HttpConfig
+        dyna = HttpConfig.build_serializer(
+            depth=2,
+            exclude=("body_type", "body_content", "data_provider", "request_type")
+        )
+
+        #self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+        self.assertEqual(stat.Meta.fields, dyna.Meta.fields)
+        self.assertEqual(stat.Meta.model, dyna.Meta.model)
+
+
+    @unittest.skip
+    def test_create_some_class(self):
+        from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import SomeClass
+        meta_some_class = SomeClass.build_some_class()
+        some_class = SomeClass
+
+        self.assertIsInstance(meta_some_class.Meta, object)
+
+
+    def test_if_dynamic_serializer_class_is_equal_to_static_metaclass(self):
+        from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
+        stat = HttpConfigSerializer
+        from MetaDataApi.dataproviders.models import HttpConfig
+        dyna = HttpConfigSerializer.build_from_metaclass()
+
+        self.assertTrue(hasattr(dyna, "url_encoded_params"))
+        self.assertTrue(hasattr(dyna, "header"))
+        self.assertTrue(hasattr(stat, "url_encoded_params"))
+        self.assertTrue(hasattr(stat, "header"))
+
+        # self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+        self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+        self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+
+    def test_if_dynamic_serializer_instance_is_equal_to_static_deserialize(self):
+        data = self.build_http_expected_json()
+        from MetaDataApi.dataproviders.serializers.HttpConfigSerializer import HttpConfigSerializer
+        stat = HttpConfigSerializer(data)
+        from MetaDataApi.dataproviders.models import HttpConfig
+        dynaClass = HttpConfig.build_serializer(
+            depth=2,
+            exclude=("body_type", "body_content", "data_provider", "request_type")
+        )
+        dyna = dynaClass(data=data)
+
+        self.assertTrue(hasattr(dyna, "url_encoded_params"))
+        self.assertTrue(hasattr(dyna, "header"))
+        self.assertTrue(hasattr(stat, "url_encoded_params"))
+        self.assertTrue(hasattr(stat, "header"))
+
+        #self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+        self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+        self.assertDictEqual(dict(stat.Meta.fields), dict(dyna.Meta.fields))
+
 
     def build_http_model_objects(self):
         from MetaDataApi.dataproviders.models import DataProvider
