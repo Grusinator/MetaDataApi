a1e7f2a1ba24946a905c533a00f59ca5e6243dd2
Grusinator <grusinator@gmail.com>
2019-12-03 14:58:22 +0100

    deleted graph app. json2model soon to join

--- MetaDataApi/graph/admin.py
+++
@@ -1,9 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from mutant import models
-
-from MetaDataApi.graph.models import Dummy
-
-admin.site.register(Dummy)
-
-admin.site.register(models.FieldDefinition)
