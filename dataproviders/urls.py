968799e252ea2c1f2c793c8b95a3d182e9694f38
Grusinator <grusinator@gmail.com>
2019-12-14 12:00:33 +0100

    added a datafile view from meta data

--- dataproviders/urls.py
+++ dataproviders/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 
 from dataproviders.views import DataProviderView, oauth2redirect_view
+from dataproviders.views.data_file_view import data_dump_view
 from dataproviders.views.endpoint_detail_view import endpoint_detail_view
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<str:provider_name>', DataProviderView.data_provider, name='provider_detail'),
     path('<str:provider_name>/endpoint/<str:endpoint_name>', endpoint_detail_view, name='endpoint_detail'),
     url(r'^oauth2redirect/$', oauth2redirect_view, name='oauth2redirect'),
+    path('data_dumps/<str:file_name>', data_dump_view, name='data_dump'),
 ]
