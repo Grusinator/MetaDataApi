522d08e9743d9905c8a5f7e5e8474bb38fd66b48
Grusinator <grusinator@gmail.com>
2019-12-15 13:10:03 +0100

    setup of circle ci test

--- scripts/delete_and_create_database.ps1
+++ scripts/delete_and_create_database.ps1
@@ -2,3 +2,8 @@ $env:PGPASSWORD = "dev1234"
 psql -h localhost -p 5432 -U django -d  "meta_data_api"  -f .\scripts\delete_and_create_database.sql
 
 pipenv run python manage.py migrate
+
+manage.py createsuperuser2 --username django --password dev1234 --noinput
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" |
+        pipenv run python manage.py shell
