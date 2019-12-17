d5a0453ab9504aaae432b1c9657c2c00589f1a01
Grusinator <grusinator@gmail.com>
2019-12-17 09:04:57 +0100

    trying to make work on circleci

--- dataproviders/tests/services/test_storeDataFromProviderService.py
+++ dataproviders/tests/services/test_storeDataFromProviderService.py
@@ -3,6 +3,9 @@ import unittest
 import django
 from django.test import TransactionTestCase
 
+from MetaDataApi.utils import JsonUtils
+from dataproviders.models import DataDump
+
 
 class TestStoreDataFromProviderService(TransactionTestCase):
     @classmethod
@@ -13,30 +16,28 @@ class TestStoreDataFromProviderService(TransactionTestCase):
     @unittest.skip(
         "excluded since it is not relevant untill it has been implemented as a dynamic model thing/interface")
     def test_store_data_from_provider_service(self):
-        pass
-        # from dataproviders.services.services import StoreDataFromProviderService
-        #
-        # from dataproviders.models.initialize_data_providers import InitializeDataProviders
-        # from metadata.models import FileAttribute
-        # from metadata.tests import LoadTestData
-        #
-        # user = LoadTestData.init_user()
-        # InitializeDataProviders.load()
-        #
-        # dpp = LoadTestData.init_strava_data_provider_profile()
-        # LoadTestData.create_dummy_provider(dpp)
-        #
-        # data = StoreDataFromProviderService.execute({
-        #     "provider_name": dpp.data_provider.provider_name,
-        #     "endpoint_name": "activity",
-        #     "user_pk": user.pk
-        # })
+        from dataproviders.services import fetch_data_from_provider
 
-        # data = JsonUtils.validate(data)
-        #
-        # fileAtt = FileAttribute.objects.get()
-        #
-        # file_as_str = fileAtt.read_as_str()
-        # file = JsonUtils.validate(file_as_str)
-        #
-        # self.assertEqual(data, file)
+        from dataproviders.models.initialize_data_providers import InitializeDataProviders
+        from metadata.tests import LoadTestData
+
+        user = LoadTestData.init_user()
+        InitializeDataProviders.load()
+
+        dpp = LoadTestData.init_strava_data_provider_profile()
+        LoadTestData.create_dummy_provider(dpp)
+
+        data = fetch_data_from_provider.fetch_data_from_provider_endpoint(
+            provider_name=dpp.data_provider.provider_name,
+            endpoint_name="activity",
+            user_pk=user.pk
+        )
+
+        data = JsonUtils.validate(data)
+
+        data_dump = DataDump.objects.get()
+
+        file_as_str = data_dump.read_as_str()
+        file = JsonUtils.validate(file_as_str)
+
+        self.assertEqual(data, file)
