1c43609dcb0502b202970c5c1cf3bf979b5cdaee
William <grusinator@gmail.com>
2020-01-25 10:41:11 +0000

    added sh scripts

---
+++ scripts/start_local_servers.sh
@@ -0,0 +1,5 @@
+sudo service postgresql start
+sudo service redis-server start
+# sudo /etc/init.d/redis-server stop
+# pg_ctl -D /usr/local/var/postgres stop
+# redis-cli shutdown
---
+++ scripts/stop_local_servers_and_release_ip_to_docker.sh
@@ -0,0 +1,5 @@
+sudo service postgresql stop
+sudo service redis-server stop
+# sudo /etc/init.d/redis-server stop
+# pg_ctl -D /usr/local/var/postgres stop
+# redis-cli shutdown
