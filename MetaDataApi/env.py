0af269eff9236fd2e55ae506deaed57b2c131ca1
grusinator <grusinator@gmail.com>
2020-01-31 16:43:31 +0000

    more production ready

--- MetaDataApi/env.py
+++ MetaDataApi/env.py
@@ -14,7 +14,7 @@ class Env(Enum):
 url_mapper = {
     # "PROD": "https://meta-data-api.herokuapp.com/",
     # "DEV": "https://meta-data-api-dev.herokuapp.com/",
-    Env.PROD: "http://metadataapi.wsh-home.dk:8000/",
+    Env.PROD: "http://metadataapi.grusinator.com/",
     Env.DEV: "http://metadataapi.wsh-home.dk:8000/",
     Env.LOCAL: "http://localhost:8000/",
     Env.TEST: "http://localhost:8000/"
