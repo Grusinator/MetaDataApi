b82768258a1b6e68ff65a6d5e96b58bc360640a6
Grusinator <grusinator@gmail.com>
2020-02-12 13:09:37 +0100

    updated a lot of scripts and pgadmin to be able to clear db in docker

--- scripts/delete_and_create_database.ps1
+++
@@ -1,7 +0,0 @@
-$env:PGPASSWORD = "dev1234"
-psql -h localhost -p 5432 -U django -d  "meta_data_api"  -f .\scripts\delete_and_create_database.sql
-
-pipenv run python manage.py migrate
-
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'dev1234')" |
-        pipenv run python manage.py shell
---
+++ scripts/docker_db_recreate_and_setup.sh
@@ -0,0 +1,7 @@
+docker cp ./scripts/ metadataapi_db:/scripts/
+
+docker exec -it metadataapi_db ./scripts/db_recreate.sh
+
+#docker exec -it meta_data_api_db psql -h localhost -p 5432 -U django -d  "meta_data_api"  -f ./scripts/db_recreate.sql
+
+docker exec -it metadataapi_django ./scripts/db_setup.sh
