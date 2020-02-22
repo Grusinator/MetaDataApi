b7351533c4a87c5f04fe71ae15cef863906b1f51
Grusinator <grusinator@gmail.com>
2020-02-22 10:28:03 +0100

    changes in view. added select box. works pretty nice now.

--- dynamic_models/views/dynamic_data_instances_view.py
+++ dynamic_models/views/dynamic_data_instances_view.py
@@ -1,18 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from json2model.services.dynamic_model.dynamic_model_utils import get_dynamic_model
+from json2model.services.dynamic_model.dynamic_model_utils import get_dynamic_model, get_all_model_definition_names
 
 from dynamic_models.views.dynamic_view_object import DynamicViewObject
 
 
+@login_required
 def dynamic_data_instances_view(request):
-    model_name = request.GET.get('model')
+    user_pk = request.user.pk
+    model_names = get_all_model_definition_names()
+    selected_model_name = request.GET.get('model')
     order_by = request.GET.get('order_by')
-    model = get_dynamic_model(model_name or "")
-
-    instances = model.objects.filter()[:100]
-    view_instances = [DynamicViewObject(inst) for inst in instances]
-    field_names = view_instances[0].field_names if model else None
-    return render(request, 'dynamic_data_instances.html',
-                  {"dynamic_data_instances": view_instances,
-                   "model_name": model_name,
-                   "field_names": field_names})
+    html_params = {"model_names": model_names, "selected_model_name": selected_model_name}
+    if selected_model_name:
+        model = get_dynamic_model(selected_model_name)
+        instances = model.objects.filter(user_pk=user_pk)[:100]
+        view_instances = [DynamicViewObject(inst) for inst in instances]
+        html_params["dynamic_data_instances"] = view_instances
+        html_params["field_names"] = view_instances[0].field_names if model else None
+    return render(request, 'dynamic_data_instances.html', html_params)
